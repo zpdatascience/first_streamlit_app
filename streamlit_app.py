4caf002bf67cda3ebdc27387e88899dc3e919c1f
zpdatascience <136375601+zpdatascience@users.noreply.github.com>
2023-06-12 15:07:54 -0400

    Update streamlit_app.py
    pick fruit names instead of indices

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
 streamlit.dataframe(my_fruit_list)
